e9ff29f2a1510ab9f9668214019518d81b3bef5d
Juan Pablo Carvajal <jcarvajalabrahams@gmail.com>
2023-12-27 21:04:07 -0600

    Add compounded factor
    Interests can be compounded by week, month, trimester, quarterly, biannually, annual. Also add basic css.

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,70 @@
 document.getElementById("amort_button").addEventListener("click", () => {
     
-    erase_table("amort_table");
-    erase_table("stats_table");
+    erase_element("amort_table");
+    erase_element("stats_table");
     
     const form = new FormData(document.getElementById("amort_form"));
-    
-    let form_values = {
-        loan: Math.abs(form.get("total_loan")),
-        periods: Math.abs(form.get("loan_term")),
-        interests: Math.abs(form.get("annual_interests"))
+    const params = get_calc_input(form);
+
+    headers = {
+        schedule: ["Período", "Intereses", "Abono", "Saldo a obligación"],
+        stats: ["Pago Mensual", "Intereses Totales", "Pago Total"]
     }
-    amortization_schedule(form_values,"amort_table","stats_table");
+
+    amortization_schedule(params, headers, "amort_table","stats_table");
 
 });
 
-function erase_table(id) {
+/** Converts a text to int else returns 0
+ * @param {string} string - string to convert to number 
+ */
+function to_int(string){
+
+    const number = parseInt(string);
+    if(number)
+        return number;
+    else
+        return 0;
+
+}
+
+function get_calc_input(form){
+
+
+    const years = to_int(form.get("term_years"));
+    const months = to_int(form.get("term_months"));
+    const weeks = to_int(form.get("term_weeks"));
+
+    const loan_term = years + (months/12) + (weeks/52);
+    console.log("Loan term:", loan_term);
+
+    const form_values = {
+        loan: Math.abs(form.get("total_loan")),
+        periods: loan_term,
+        interests: Math.abs(form.get("annual_interests")),
+        compounded: form.get("compounded")
+    }
+
+    return form_values;
+}
+
+/** Erases an element with id if it exists
+ * @param {string} id - id of the element to erase
+*/
+function erase_element(id) {
     
     table = document.getElementById(id);
     if(table !== null)
         table.remove();
-
 }
 
+
+/** Creates an html table an populates it with data
+ * @param {Array} headers  - headers of the table
+ * @param {Object[]} data     - array of objects to populate the table
+ * @param {string} id      - id to assign to the new table
+ * 
+*/
 function create_table(headers, data, id) {
 
     const table = document.createElement("table");
@@ -49,10 +92,14 @@ function create_table(headers, data, id) {
 
 }
 
-function amortization_schedule(form, table_id, stats_id) {
+function amortization_schedule(form, headers, table_id, stats_id) {
 
-    const mp = monthly_payments(form.loan, form.periods, form.interests);
-    const data = amortization_data(form.loan, form.periods, form.interests/1200, mp);
+    const term = form.periods * compounded_factor(form.compounded);
+    const interests = form.interests/(100*compounded_factor(form.compounded));
+
+    const mp = payments(form.loan, form.periods, form.interests, form.compounded);
+    const data = amortization_data(form.loan, term, interests, mp);
+
     const total_interests = data.reduce((sum, cell) => {
         return sum + cell.interest}, 0);
     const total_payment = data.reduce((sum, cell) => {
@@ -64,11 +111,10 @@ function amortization_schedule(form, table_id, stats_id) {
         total_payment: Math.round(total_payment * 100) / 100,
     }];
     
-    schedule_headers = ["Mes", "Intereses", "Abono", "Saldo a obligación"];
-    stats_headers = ["Pago Mensual", "Intereses Totales", "Pago Total"];
+   
     
-    schedule = create_table(schedule_headers, data, table_id);
-    stats = create_table(stats_headers, stats_data, stats_id);
+    schedule = create_table(headers.schedule, data, table_id);
+    stats = create_table(headers.stats, stats_data, stats_id);
     
     const amort_div = document.getElementById("amort_div");
     amort_div.appendChild(stats);
@@ -76,7 +122,7 @@ function amortization_schedule(form, table_id, stats_id) {
 
 }
 
-function amortization_data(loan, periods, monthly_interest, mp) {
+function amortization_data(loan, periods, interest, mp) {
     
     let data = [];
     let beginning_balance = loan;
@@ -89,7 +135,7 @@ function amortization_data(loan, periods, monthly_interest, mp) {
         let data_point = create_data_point(i, interest_to_pay, principal, ending_balance);
         data.push(data_point);
     
-        interest_to_pay = (monthly_interest * beginning_balance);
+        interest_to_pay = (interest * beginning_balance);
         principal = (mp - interest_to_pay);
         ending_balance = (beginning_balance - principal);
         beginning_balance = ending_balance;
@@ -109,12 +155,32 @@ function create_data_point(period, interest, principal, ending_balance) {
 
 }
 
-function monthly_payments(principal, term, interests) {
+function compounded_factor(compounded) {
+    switch (compounded) {
+        case "annual":
+            return 1;
+        case "biannualy":
+            return 2;    
+        case "quarterly":
+            return 3;
+        case "trimester":
+            return 4;
+        case "month":
+            return 12;   
+        case "weekly":
+            return 52;
+
+    }
+}
+
+function payments(principal, term, interests, compounded) {
     
+    let factor = compounded_factor(compounded);
+ 
     const P = principal;
-    const i = interests/1200
-    const n = term;
-    
+    const i = interests/(100*factor);
+    const n = factor*term;
+
     const A = P * ((i*(1+i)**n)/(((1+i)**n) - 1));
     
     return A
